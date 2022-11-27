9771a9dc3b07624870b07767a291ff253f1102fb
nhuanh9 <chopperlouis@gmail.com>
2022-11-27 15:20:05 +0700

    demo1: xong khái niệm component

---
+++ src/pages/Admin.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+export default function Admin() {
+    return (
+        <>
+            <Navbar></Navbar>
+            <hr/>
+            <h1>Admin Page</h1>
+            <Footer></Footer>
+        </>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+export default function Home() {
+    return (
+        <>
+
+            <Header></Header>
+            <Navbar></Navbar>
+            <hr/>
+            <h1>Home Page</h1>
+            <Footer></Footer>
+        </>
+    )
+}
